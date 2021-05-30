634105ef7cd66bb9a7960cc78e4f736d46b9c222
joesanthosh15@gmail.com <joe.santhosh88@yahoo.com>
2021-05-30 15:05:52 -0400

    Added a navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
-  <div id="app">Hello</div>
+  <div id="app">
+    <app-navbar></app-navbar>
+  </div>
   <router-view />
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue";
+
 export default {
-  data() {},
+  components: {
+    appNavbar: Navbar,
+  },
 };
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
+
 * {
   margin: 0;
+  font-family: "Montserrat", sans-serif;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar">
+    <ul>
+      <li class="navbar__home">VueCommerce</li>
+      <div class="navbar__items">
+        <li class="navbar__item">Login</li>
+        <li class="navbar__item">Sign Up</li>
+        <li class="navbar__item">Cart</li>
+      </div>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+.navbar {
+  background-color: rgba(39, 209, 82, 0.7);
+  padding: 20px;
+}
+
+.navbar > ul {
+  list-style-type: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.navbar__items {
+  display: flex;
+  align-items: center;
+}
+
+.navbar__item {
+  margin-right: 20px;
+}
+</style>
