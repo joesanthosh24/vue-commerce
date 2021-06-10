d68ab1885145c47476a591e1f4915d5556178237
joesanthosh15@gmail.com <joe.santhosh88@yahoo.com>
2021-06-10 09:29:50 -0400

    Added i18n localization library for language translations

--- src/main.js
+++ src/main.js
@@ -7,14 +7,17 @@ import store from "./store";
 
 import VeeValidatorPlugin from "./includes/validation";
 import { auth } from "./includes/firebase";
+
 import Icon from "./directives/icon";
+
+import i18n from "./includes/i18n";
 
 // set a variable which will be set to createApp() if app isn't initialized in onAuthStateChanged()
 let app;
 
 auth.onAuthStateChanged(() => {
   if (!app) {
-    app = createApp(App);
+    app = createApp(App).use(i18n);
 
     app.use(store);
     app.use(router);
